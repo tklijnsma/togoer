c1cea5c9a366306db56d4276fbee999fd7574b4d
Thomas on GC <tklijnsm@gmail.com>
2020-02-26 21:55:24 +0000

    script must be sourced in order to use aliases and such

--- activate.sh
+++ activate.sh
@@ -1,3 +1,3 @@
 TOGOERDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 complete -C "${TOGOERDIR}/togoer-completion.py" to
-alias to="${TOGOERDIR}/to"
+alias to=". ${TOGOERDIR}/to"
